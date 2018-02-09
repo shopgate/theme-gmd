60969448913d0c70773cea898c56e4af2a55bff0
Szymon Nowicki <szymon.nowicki@shopgate.com>
2018-02-09 12:42:22 +0100

    Git branch name or travis one.

--- travis/modules/Git.js
+++ travis/modules/Git.js
@@ -11,16 +11,48 @@ const Cmd = require('./Cmd');
  */
 class Git extends Cmd {
   /**
+   * Checks if branch which is fetched from git looks correct.
+   * @param {string} branchName Branch name from git.
+   * @returns {Promise}
+   */
+  static shouldGetTravisBranch(branchName) {
+    return new Promise((resolve, reject) => {
+      if (branchName.indexOf('HEAD') === -1) {
+        return resolve();
+      }
+      return reject();
+    });
+  }
+
+  /**
+   * Gets travis specific branch (fetched from ENV).
+   * @returns {Promise}
+   */
+  static getTravisBranch() {
+    return this.runCommand('echo $TRAVIS_PULL_REQUEST_BRANCH');
+  }
+  /**
    * Gets current branch.
    * @returns {number}
    */
   static getBranch() {
     return new Promise((resolve, reject) => {
+      let branchFromGit;
       this.runCommand('git branch | grep \\*')
         .then((data) => {
-          resolve(data.replace('* ', ''));
+          branchFromGit = data.replace('* ', '');
+          return this.shouldGetTravisBranch(branchFromGit);
         })
-        .catch(err => reject(err));
+        .then(() => this.getTravisBranch())
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(branchFromGit);
+        });
     });
   }
 }
